f52dff88396283604534b7b1e4a42bae1ebbaf8d
kim junsoo <kjs201105@naver.com>
2026-02-25 16:14:36 +0900

    Feat: post page

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,12 @@
+export default function PostsPage() {
+  return (
+    <div className="container mx-auto text-center p-8">
+      <h1>포스트 리스트</h1>
+      <div>
+        <ul>게시글1</ul>
+        <ul>게시글2</ul>
+        <ul>게시글3</ul>
+      </div>
+    </div>
+  );
+}
